ca7832dc901cf3dacb6961c64be6e2a65fe156d9
Fasna Mohammed Khalid <200470534@student.georgianc.on.ca>
2022-06-23 09:03:39 -0400

    Attributes , constructors and getters and setters created

--- src/main/java/com/example/midtermexam200470534/Student.java
+++ src/main/java/com/example/midtermexam200470534/Student.java
@@ -1,6 +1,83 @@
 package com.example.midtermexam200470534;
 
 public class Student {
+    private int  studentNumber;
+    private String firstName, lastName,gender;
+    private int telephoneNumber,averageGrade;
+    private String province,majorCode;
 
-    
+    public Student(int studentNumber, String firstName, String lastName, String gender, int telephoneNumber, int averageGrade, String province, String majorCode) {
+        this.studentNumber = studentNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.telephoneNumber = telephoneNumber;
+        this.averageGrade = averageGrade;
+        this.province = province;
+        this.majorCode = majorCode;
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(int telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public int getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(int averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getMajorCode() {
+        return majorCode;
+    }
+
+    public void setMajorCode(String majorCode) {
+        this.majorCode = majorCode;
+    }
 }
